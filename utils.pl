6e6bec1171634233e8491627db6b886a5616d775
Zé Diogo <zediogox@hotmail.com>
2022-12-21 17:09:48 +0000

    function to check if movement is diagonal tested

--- utils.pl
+++ utils.pl
@@ -9,24 +9,36 @@ is_between(Lower, Upper, X) :-
 
 % INPUT VALIDATION
 
-validate_move_board_element_indices(StartRow, StartColumn, EndRow, EndColumn) :-
+validate_indices(StartRow, StartColumn, EndRow, EndColumn) :-
     is_between(1, 12, StartRow),
     is_between(1, 12, StartColumn),
     is_between(1, 12, EndRow),
     is_between(1, 12, EndColumn), !.
-validate_move_board_element_indices(StartRow, _, _, _) :-
+validate_indices(StartRow, _, _, _) :-
     \+ is_between(1, 12, StartRow),
     write('Error: invalid start row index. Start row index must be between 1 and 12.'), nl,
     fail.
-validate_move_board_element_indices(_, StartColumn, _, _) :-
+validate_indices(_, StartColumn, _, _) :-
     \+ is_between(1, 12, StartColumn),
     write('Error: invalid start column index. Start column index must be between 1 and 12.'), nl,
     fail.
-validate_move_board_element_indices(_, _, EndRow, _) :-
+validate_indices(_, _, EndRow, _) :-
     \+ is_between(1, 12, EndRow),
     write('Error: invalid end row index. End row index must be between 1 and 12.'), nl,
     fail.
-validate_move_board_element_indices(_, _, _, EndColumn) :-
+validate_indices(_, _, _, EndColumn) :-
     \+ is_between(1, 12, EndColumn),
     write('Error: invalid end column index. End column index must be between 1 and 12.'), nl,
     fail.
+
+%---------------------------------------------------------------------------%
+validate_not_diagonal(StartRow, StartColumn, EndRow, EndColumn) :-
+    (StartRow = EndRow, StartColumn = EndColumn ->
+     write('Error: start position and end position are the same. Move must be to a different position.'), nl,
+     fail ;
+     true),
+    (abs(StartRow - EndRow) \= abs(StartColumn - EndColumn) ->
+     true ;
+     write('Error: move cannot be diagonal.'), nl,
+     fail).
+    
