aa274f3e9479558e8b5f32534e2f7b9e9e284d11
Zé Diogo <zediogox@hotmail.com>
2022-12-22 17:57:01 +0000

    moved around some functions

--- game_logic.pl
+++ game_logic.pl
@@ -54,7 +54,6 @@ is_column_range_clear_greater(StartColumn, EndColumn, Row) :-
     BoardElement = board_piece(_, _, ' ', _, _),
     is_column_range_clear_greater(StartColumn1, EndColumn, Row).
 
-
 check_row_range(StartRow, EndRow, Column) :-
     SR is 13 - StartRow, ER is 13 - EndRow,
     SR < ER,
@@ -78,9 +77,41 @@ is_row_range_clear_greater(StartRow, EndRow, Column) :-
     BoardElement = board_piece(_, _, ' ', _, _),
     is_row_range_clear_greater(StartRow1, EndRow, Column).
 
+% ------------------------ VALIDATE MOVE ------------------------
+
+validate_not_diagonal(StartRow, StartColumn, EndRow, EndColumn) :-
+    (StartRow = EndRow, StartColumn = EndColumn ->
+     write('Error: start position and end position are the same. Move must be to a different position.'), nl,
+     fail ;
+     true),
+    (abs(StartRow - EndRow) \= abs(StartColumn - EndColumn) ->
+     true ;
+     write('Error: move cannot be diagonal.'), nl,
+     fail).
+
+validate_indices(StartRow, StartColumn, EndRow, EndColumn) :-
+    is_between(1, 12, StartRow),
+    is_between(1, 12, StartColumn),
+    is_between(1, 12, EndRow),
+    is_between(1, 12, EndColumn), !.
+validate_indices(StartRow, _, _, _) :-
+    \+ is_between(1, 12, StartRow),
+    write('Error: invalid start row index. Start row index must be between 1 and 12.'), nl,
+    fail.
+validate_indices(_, StartColumn, _, _) :-
+    \+ is_between(1, 12, StartColumn),
+    write('Error: invalid start column index. Start column index must be between 1 and 12.'), nl,
+    fail.
+validate_indices(_, _, EndRow, _) :-
+    \+ is_between(1, 12, EndRow),
+    write('Error: invalid end row index. End row index must be between 1 and 12.'), nl,
+    fail.
+validate_indices(_, _, _, EndColumn) :-
+    \+ is_between(1, 12, EndColumn),
+    write('Error: invalid end column index. End column index must be between 1 and 12.'), nl,
+    fail.
+
 % ------------------------ END BOUNDARIES CHECK ------------------------
-    
 
-
-
+% move
 
--- utils.pl
+++ utils.pl
@@ -7,48 +7,6 @@ is_between(Lower, Upper, X) :-
     
 %---------------------------------------------------------------------------%
 
-% INPUT VALIDATION
-
-validate_indices(StartRow, StartColumn, EndRow, EndColumn) :-
-    is_between(1, 12, StartRow),
-    is_between(1, 12, StartColumn),
-    is_between(1, 12, EndRow),
-    is_between(1, 12, EndColumn), !.
-validate_indices(StartRow, _, _, _) :-
-    \+ is_between(1, 12, StartRow),
-    write('Error: invalid start row index. Start row index must be between 1 and 12.'), nl,
-    fail.
-validate_indices(_, StartColumn, _, _) :-
-    \+ is_between(1, 12, StartColumn),
-    write('Error: invalid start column index. Start column index must be between 1 and 12.'), nl,
-    fail.
-validate_indices(_, _, EndRow, _) :-
-    \+ is_between(1, 12, EndRow),
-    write('Error: invalid end row index. End row index must be between 1 and 12.'), nl,
-    fail.
-validate_indices(_, _, _, EndColumn) :-
-    \+ is_between(1, 12, EndColumn),
-    write('Error: invalid end column index. End column index must be between 1 and 12.'), nl,
-    fail.
-
-
-%---------------------------------------------------------------------------%
-
-% GAME LOGIC VALIDATION
-
-validate_not_diagonal(StartRow, StartColumn, EndRow, EndColumn) :-
-    (StartRow = EndRow, StartColumn = EndColumn ->
-     write('Error: start position and end position are the same. Move must be to a different position.'), nl,
-     fail ;
-     true),
-    (abs(StartRow - EndRow) \= abs(StartColumn - EndColumn) ->
-     true ;
-     write('Error: move cannot be diagonal.'), nl,
-     fail).
-
-
-%---------------------------------------------------------------------------%
-
 
 atomic_list_concat(Atom,Separator,Res) :- atom_chars(Atom,CAtom),
                                           atomic_list_concat_(CAtom,Separator,List),
